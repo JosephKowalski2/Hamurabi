f157ec4d57e01e22d302d856cfcad55069c8b0f8
Joseph Kowalski <jkjr25@gmail.com>
2022-03-11 21:40:59 -0500

    Added harvest and graineatenbyrats

--- src/main/java/hammurabi/Hammurabi.java
+++ src/main/java/hammurabi/Hammurabi.java
@@ -5,12 +5,21 @@ import java.util.Scanner;
 public class Hammurabi {         // must save in a file named Hammurabi.java
     Random rand = new Random();  // this is an instance variable
     Scanner scanner = new Scanner(System.in);
+    int population = 100;
+    int bushels = 2800;
+    int acres = 1000;
+    int landValue = 19;
+
 
     public static void main(String\[\] args) { // required in every Java program
         new Hammurabi().playGame();
     }
 
     void playGame() {
+        int population = 100;
+        int bushels = 2800;
+        int acres = 1000;
+        int landValue = 19;
         // declare local variables here: grain, population, etc.
         // statements go after the declations
     }
